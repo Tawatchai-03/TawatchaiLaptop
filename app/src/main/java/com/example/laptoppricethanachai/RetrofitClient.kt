6a77c289c8c70d49993531917c0affdb1c3412a3
Tawatchai-03 <tawatchai.kee@rmutto.ac.th>
2024-09-19 16:10:21 +0700

    first

--- app/src/main/java/com/example/laptoppricethanachai/RetrofitClient.kt
+++ app/src/main/java/com/example/laptoppricethanachai/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://192.168.1.70:4000/"
+    private const val BASE_URL = "http://10.13.3.96:3000/"
 
     private var retrofit: Retrofit? = null
 
